ac537305b144a93d09f6a1f0d6d6041efdea25f5
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-06-20 02:35:52 -0600

    Terminando apartado de employee

--- src/app/employee/page.jsx
+++ src/app/employee/page.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import EmployeeService from "@/services/rrhh/employees/employeeService";
 import 'devextreme/dist/css/dx.light.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import DataGrid, { Column, Editing, Popup, Paging, Form, Pager, Item } from "devextreme-react/data-grid";
+import DataGrid, { Column, Editing, Popup, Paging, Form, Pager, Item, SearchPanel, FilterRow, Scrolling } from "devextreme-react/data-grid";
 import ResponseHandler from '@/module/handler/responseHandler';
+import Messages from '@/template/component/messages';
 import { LoadPanel } from 'devextreme-react/load-panel';
 
-//Número de registros a mostrar en paginación de grid
-const pageSizes = [5, 10, 15, 25, 50, 100];
+
+const allowedPageSizes = [5, 10, 20];
 
 class Employee extends React.Component {
     
@@ -54,23 +55,24 @@ class Employee extends React.Component {
         this.request.addEmployee(data)
             .then(ok => {
                 this.getEmployee(); // Refresca la lista de empleados
-                ResponseHandler.success('Employee added successfully');
+                ResponseHandler.success('Empleado agregado exitosamente');
             })
             .catch(error => {
-                console.error('Error adding employee:', error);
+                console.error('Error agregando empleado:', error);
                 ResponseHandler.error(error);
-                console.log(error);
             });
     }
 
-    updateEmployee = (id, data) => {
-        this.request.updateEmployee(id, data)
+    updateEmployee = (id, newData) => {
+        const oldData = this.state.employees.find(emp => emp.id === id);
+        const updatedData = { ...oldData, ...newData }; // Fusiona los datos antiguos con los nuevos
+        this.request.updateEmployee(id, updatedData)
             .then(ok => {
                 this.getEmployee(); // Refresca la lista de empleados
-                ResponseHandler.success('Employee updated successfully');
+                ResponseHandler.success(' exitosamente');
             })
             .catch(error => {
-                console.error('Error updating employee:', error);
+                console.error('Error actualizando empleado:', error);
                 ResponseHandler.error(error);
             });
     }
@@ -79,10 +81,10 @@ class Employee extends React.Component {
         this.request.deleteEmployee(id)
             .then(ok => {
                 this.getEmployee(); // Refresca la lista de empleados
-                ResponseHandler.success('Employee deleted successfully');
+                
             })
             .catch(error => {
-                console.error('Error deleting employee:', error);
+                console.error('Error eliminando empleado:', error);
                 ResponseHandler.error(error);
             });
     }
@@ -127,34 +129,36 @@ class Employee extends React.Component {
                         onFocusedRowChanged={(e) => {
                             this.setState({ focusedRowKey: e.row?.id });
                         }}
-                        // onEditingStart={() => alert("EditingStart")}
-                        onSaving={(e) => {
-                            if (window.confirm("¿Estás seguro de que quieres guardar los cambios?")) {
-                                const cambios = e.changes[0].data; // Obtener los cambios realizados
-                                const key = e.changes[0].key; // Obtener la key de la tarea
-                                if (key && typeof key === "string" && key.startsWith("_DX_KEY_")) {
-                                    // Es una inserción
-                                    this.addEmployee(cambios);
+                        onSaving={async (e) => {
+                            // Confirmar solo para guardado (inserción o actualización)
+                            if (e.changes && e.changes.length > 0) {
+                                const confirm = await Messages.confirm("¿Estás seguro de que quieres guardar los cambios?", "Guardar");
+                                if (confirm) {
+                                    const cambios = e.changes[0].data; // Obtener los cambios realizados
+                                    const key = e.changes[0].key; // Obtener la key de la tarea
+                                    if (key && typeof key === "string" && key.startsWith("_DX_KEY_")) {
+                                        // Es una inserción
+                                        this.addEmployee(cambios);
+                                    } else {
+                                        // Es una actualización
+                                        const id = key;
+                                        this.updateEmployee(id, cambios);
+                                    }
                                 } else {
-                                    // Es una actualización
-                                    const id = key;
-                                    this.updateEmployee(id, cambios);
+                                    e.cancel = true; // Cancelar la operación si el usuario cancela la confirmación
                                 }
-                            } else {
-                                e.cancel = true; // Cancelar la operación si el usuario cancela la confirmación
                             }
                         }}
-                        onRowRemoving={(e) => {
-                            if (window.confirm("¿Estás seguro de que quieres eliminar este registro?")) {
-                                this.deleteEmployee(e.data.id);
-                            } else {
-                                e.cancel = true; // Cancela la eliminación si el usuario cancela la confirmación
-                            }
+      
+                        onRowRemoving={async (e) => {
+                            // Ejecutar eliminación sin confirmación personalizada
+                            this.deleteEmployee(e.data.id);
                         }}
-                        onRowRemoved={() => alert("Eliminacion confirmada")}
                     >
+                        <Scrolling mode="standard" />
                         <Paging defaultPageSize={10} />
-                        <Pager showPageSizeSelector={true} allowedPageSizes={pageSizes} showInfo={true} />
+                        <Pager showPageSizeSelector={true} allowedPageSizes={allowedPageSizes} showInfo={true} />
+                        <SearchPanel visible={true} highlightCaseSensitive={true} placeholder='Buscar empleado'/>
                         <Column dataField="id" caption="ID" />
                         <Column dataField="first_name" caption="First Name" />
                         <Column dataField="last_name" caption="Last Name" />
@@ -163,26 +167,24 @@ class Employee extends React.Component {
                         <Column dataField="job_title" caption="Job Title" />
                         <Column dataField="salary" caption="Salary" />
                         <Column dataField="created_at" caption="Created At" dataType="date" />
+                        <FilterRow visible={true} />
                         <Editing
-                            useIcons
                             mode="popup"
+                            useIcons
                             allowAdding={true}
                             allowUpdating={true}
                             allowDeleting={true}
                             onRowInserting={(e) => this.addEmployee(e.data)}
                             onRowUpdating={(e) => this.updateEmployee(e.key, e.newData)}
-                            onRowRemoving={(e) => this.deleteEmployee(e.key)}
                         >
-                            <Popup title="Employee Info" showTitle={true} width={700} height={325} />
+                            <Popup title="Employee Info" showTitle={true} width={700} height={525} />
                             <Form>
-                            <Item itemType="group" colCount={2} colSpan={2}>
                                 <Item dataField="first_name" />
                                 <Item dataField="last_name" />
                                 <Item dataField="email" />
                                 <Item dataField="phone" />
                                 <Item dataField="job_title" />
                                 <Item dataField="salary" />
-                                </Item>
                             </Form>
                         </Editing>
                     </DataGrid>
--- src/module/handler/responseHandler.js
+++ src/module/handler/responseHandler.js
@@ -1,46 +1,40 @@
-
 import Messages from "@/template/component/messages";
-
-// src/module/handler/responseHandler.js
-
-// src/module/handler/responseHandler.js
 
 class ResponseHandler {
     static error(error) {
         console.error(error); // Asegúrate de que el error completo se registre en la consola
 
         if (!error.response) {
-            alert('Se ha producido un error inesperado. Detalles: ' + error.message);
+            Messages.notify('Se ha producido un error inesperado. Detalles: ' + error.message, 'error');
             return;
         }
 
         if (error.response.status === 413) {
-            alert('El tamaño de los archivos adjuntos excede el máximo permitido de 7MB, quite o reduzca el tamaño de los adjuntos.');
+            Messages.notify('El tamaño de los archivos adjuntos excede el máximo permitido de 7MB, quite o reduzca el tamaño de los adjuntos.', 'error');
             return;
         }
 
         if (error.response.status === 400) {
-            alert('Error en la solicitud. Por favor revise los datos enviados.');
+            Messages.notify('Error en la solicitud. Por favor revise los datos enviados.', 'error');
             return;
         }
 
         if (error.response.status === 404) {
-            alert('Recurso no encontrado.');
+            Messages.notify('Recurso no encontrado.', 'error');
             return;
         }
 
         if (error.response.status === 500) {
-            alert('Error interno del servidor. Por favor intente nuevamente más tarde.');
+            Messages.notify('Error interno del servidor. Por favor intente nuevamente más tarde.', 'error');
             return;
         }
 
-        alert('Se ha producido un error inesperado.');
+        Messages.notify('Se ha producido un error inesperado.', 'error');
     }
 
     static success(message) {
-        alert(message);
+        Messages.notify(message, 'success');
     }
 }
 
 export default ResponseHandler;
-
